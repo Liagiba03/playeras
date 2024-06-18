b9ebadf3c23336c3f778694c03dbf5e3625e29f4
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-06-17 21:38:00 -0600

    changes in the session 2

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true, // true si usas HTTPS
-        httpOnly: true,
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24,
         sameSite: 'None'
     }
