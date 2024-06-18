22ffe2063557cd76be6a76060278813f7473d766
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-06-17 22:42:24 -0600

    para cookies 3

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
     origin: 'https://kitsune-negocio.netlify.app',
     methods: ["POST", "GET", "DELETE"],
     credentials: true
-}))
+}));
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json({ limit: '16mb' }));
