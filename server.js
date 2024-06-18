f79dc93d982c9ee04c8ed93ce34aec81b2e1f8bc
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-06-17 18:36:39 -0600

    url directa

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(cors({
-    origin: [`${ORIGIN}`],
+    origin: ['https://kitsune-negocio.netlify.app'],
     methods: ["POST", "GET", "DELETE"],
     credentials: true
 }))
