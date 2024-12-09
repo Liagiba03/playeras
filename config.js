3d486feeb9fbb34be02c1712bddd8e50d64a724a
Suseth Abigail Sandoval Damian <143125742+Liagiba03@users.noreply.github.com>
2024-12-09 16:06:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || //PORT,
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_USER: process.env.DB_USER ||  "root",
-    DB_PASSWORD: process.env.DB_PASSWORD || "SusSand#",
+    DB_PASSWORD: process.env.DB_PASSWORD || //PASSWORD,
     DB_NAME: process.env.DB_NAME || "negocio",
-    DB_PORT : process.env.DB_PORT || 3306,
-    ORIGIN: process.env.ORIGIN || "http://localhost:3000",
+    DB_PORT : process.env.DB_PORT || //PORT DB,
+    ORIGIN: process.env.ORIGIN || //"http://localhost:3000",
     MYSQL_PRIVATE_URL: process.env.MYSQL_PRIVATE_URL
   };
